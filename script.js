8cd7c8b8ca690df3689376becb8b887ca4423223
Oleksandr Rudenko <o.rudenko@olearis.com>
2023-12-20 16:51:58 +0000

    init

--- script.js
+++ script.js
@@ -1,120 +1,115 @@
 (() => {
   // SpeechRecognition start
-
   var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
   var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
   var SpeechRecognitionEvent =
     SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-  var testBtn = document.querySelector(".button.test");
+  var recordBtn = document.querySelector(".button.test");
 
   const words = ["selfie", "photo", "picture"];
 
-  function testSpeech() {
-    testBtn.disabled = true;
-    testBtn.textContent = "Test in progress";
+  var recognition = new SpeechRecognition();
+  if (SpeechGrammarList) {
+    // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
+    // This code is provided as a demonstration of possible capability. You may choose not to use it.
+    var speechRecognitionList = new SpeechGrammarList();
+    var grammar =
+      "#JSGF V1.0; grammar words; public <word> = " + words.join(" | ") + " ;";
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+  }
+  recognition.continuous = false;
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
 
-    var recognition = new SpeechRecognition();
-    if (SpeechGrammarList) {
-      // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
-      // This code is provided as a demonstration of possible capability. You may choose not to use it.
-      var speechRecognitionList = new SpeechGrammarList();
-      var grammar =
-        "#JSGF V1.0; grammar words; public <word> = " +
-        words.join(" | ") +
-        " ;";
-      speechRecognitionList.addFromString(grammar, 1);
-      recognition.grammars = speechRecognitionList;
-    }
-    recognition.continuous = false;
-    recognition.lang = "en-US";
-    recognition.interimResults = false;
-    recognition.maxAlternatives = 1;
-
+  function startVoiceRecord() {
+    recordBtn.disabled = true;
+    recordBtn.textContent = "Test in progress";
     recognition.start();
-
-    recognition.onresult = function (event) {
-      // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-      // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-      // It has a getter so it can be accessed like an array
-      // The first [0] returns the SpeechRecognitionResult at position 0.
-      // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-      // These also have getters so they can be accessed like arrays.
-      // The second [0] returns the SpeechRecognitionAlternative at position 0.
-      // We then return the transcript property of the SpeechRecognitionAlternative object
-      var speechResult = event.results[0][0].transcript.toLowerCase();
-      console.log("sr", speechResult);
-      for (let i = 0; i < words.length; i++) {
-        if (speechResult.toLowerCase().includes(words[i].toLowerCase())) {
-          takepicture();
-          break;
-        }
-      }
-      // diagnosticPara.textContent = "Speech received: " + speechResult + ".";
-      // if (speechResult === phrase) {
-      //   resultPara.textContent = "I heard the correct phrase!";
-      //   resultPara.style.background = "lime";
-      // } else {
-      //   resultPara.textContent = "That didn't sound right.";
-      //   resultPara.style.background = "red";
-      // }
-
-      console.log("Confidence: " + event.results[0][0].confidence);
-    };
-
-    recognition.onspeechend = function () {
-      recognition.stop();
-      testBtn.disabled = false;
-      testBtn.textContent = "Start new test";
-    };
-
-    recognition.onerror = function (event) {
-      testBtn.disabled = false;
-      testBtn.textContent = "Start new test";
-    };
-
-    recognition.onaudiostart = function (event) {
-      //Fired when the user agent has started to capture audio.
-      console.log("SpeechRecognition.onaudiostart");
-    };
-
-    recognition.onaudioend = function (event) {
-      //Fired when the user agent has finished capturing audio.
-      console.log("SpeechRecognition.onaudioend");
-    };
-
-    recognition.onend = function (event) {
-      //Fired when the speech recognition service has disconnected.
-      console.log("SpeechRecognition.onend");
-    };
-
-    recognition.onnomatch = function (event) {
-      //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      console.log("SpeechRecognition.onnomatch");
-    };
-
-    recognition.onsoundstart = function (event) {
-      //Fired when any sound — recognisable speech or not — has been detected.
-      console.log("SpeechRecognition.onsoundstart");
-    };
-
-    recognition.onsoundend = function (event) {
-      //Fired when any sound — recognisable speech or not — has stopped being detected.
-      console.log("SpeechRecognition.onsoundend");
-    };
-
-    recognition.onspeechstart = function (event) {
-      //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log("SpeechRecognition.onspeechstart");
-    };
-    recognition.onstart = function (event) {
-      //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      console.log("SpeechRecognition.onstart");
-    };
   }
 
-  // testBtn.addEventListener("click", testSpeech);
-  testSpeech();
+  recognition.onresult = function (event) {
+    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+    // It has a getter so it can be accessed like an array
+    // The first [0] returns the SpeechRecognitionResult at position 0.
+    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+    // These also have getters so they can be accessed like arrays.
+    // The second [0] returns the SpeechRecognitionAlternative at position 0.
+    // We then return the transcript property of the SpeechRecognitionAlternative object
+    var speechResult = event.results[0][0].transcript.toLowerCase();
+    console.log("sr", speechResult);
+    for (let i = 0; i < words.length; i++) {
+      if (speechResult.toLowerCase().includes(words[i].toLowerCase())) {
+        takepicture();
+        break;
+      }
+    }
+    // diagnosticPara.textContent = "Speech received: " + speechResult + ".";
+    // if (speechResult === phrase) {
+    //   resultPara.textContent = "I heard the correct phrase!";
+    //   resultPara.style.background = "lime";
+    // } else {
+    //   resultPara.textContent = "That didn't sound right.";
+    //   resultPara.style.background = "red";
+    // }
+
+    console.log("Confidence: " + event.results[0][0].confidence);
+  };
+
+  recognition.onspeechend = function () {
+    recognition.stop();
+    recordBtn.disabled = false;
+    recordBtn.textContent = "Voice";
+  };
+
+  recognition.onerror = function (event) {
+    recordBtn.disabled = false;
+    recordBtn.textContent = "Voice";
+  };
+
+  recognition.onaudiostart = function (event) {
+    //Fired when the user agent has started to capture audio.
+    console.log("SpeechRecognition.onaudiostart");
+  };
+
+  recognition.onaudioend = function (event) {
+    //Fired when the user agent has finished capturing audio.
+    console.log("SpeechRecognition.onaudioend");
+  };
+
+  recognition.onend = function (event) {
+    //Fired when the speech recognition service has disconnected.
+    console.log("SpeechRecognition.onend");
+  };
+
+  recognition.onnomatch = function (event) {
+    //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+    console.log("SpeechRecognition.onnomatch");
+  };
+
+  recognition.onsoundstart = function (event) {
+    //Fired when any sound — recognisable speech or not — has been detected.
+    console.log("SpeechRecognition.onsoundstart");
+  };
+
+  recognition.onsoundend = function (event) {
+    //Fired when any sound — recognisable speech or not — has stopped being detected.
+    console.log("SpeechRecognition.onsoundend");
+  };
+
+  recognition.onspeechstart = function (event) {
+    //Fired when sound that is recognised by the speech recognition service as speech has been detected.
+    console.log("SpeechRecognition.onspeechstart");
+  };
+  recognition.onstart = function (event) {
+    //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
+    console.log("SpeechRecognition.onstart");
+  };
+
+  // recordBtn.addEventListener("click", testSpeech);
 
   // SpeechRecognition end
 
@@ -126,7 +121,9 @@
   let video = null;
   let canvas = null;
   let photo = null;
-  let startbutton = null;
+  const content = document.querySelector(".content");
+  const trymore = document.querySelector(".trymore");
+  let record = false;
 
   function showViewLiveResultButton() {
     if (window.self !== window.top) {
@@ -151,6 +148,7 @@
     canvas = document.getElementById("canvas");
     photo = document.getElementById("photo");
     startbutton = document.getElementById("startbutton");
+    record = true;
 
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
@@ -160,6 +158,7 @@
       })
       .catch((err) => {
         console.error(`An error occurred: ${err}`);
+        record = false;
       });
 
     video.addEventListener(
@@ -229,7 +228,15 @@
     }
   }
 
+  function startRecord() {
+    content.style.display = "block";
+    trymore.style.display = "none";
+    !record && startup();
+    startVoiceRecord();
+    // testSpeech();
+  }
+
   // Set up our event listener to run the startup process
   // once loading is complete.
-  window.addEventListener("load", startup, false);
+  recordBtn.addEventListener("click", startRecord, false);
 })();
